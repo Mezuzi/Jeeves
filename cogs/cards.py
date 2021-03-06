15657d998ec514b47dc3ff1d5a1d1c60bb9c966b
Mezuzi <oyin.syn@gmail.com>
2021-03-06 11:32:24 -0500

    fix formatting

--- cogs/cards.py
+++ cogs/cards.py
@@ -131,7 +131,7 @@ class CardCog(commands.Cog):
                 if 'influence_limit' in card and 'minimum_deck_size' in card else ""}"""
         ]
         return (', '.join(filter(None, headers)), f" {'●'*card['faction_cost']}{'○'*(5-card['faction_cost'])}"
-            if 'faction_cost' in card and 'type_code' in card and (card['type_code'] == 'identity' or card['type_code'] == 'agenda')
+            if 'faction_cost' in card and 'type_code' in card and (card['type_code'] != 'identity' and card['type_code'] != 'agenda')
             else '')
 
     def generate_color_for_faction(self, faction: str) -> discord.Color:
